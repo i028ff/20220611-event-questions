6297be00b07e1e60a153968ec02e52d5633aa680
i028ff <77201225+i028ff@users.noreply.github.com>
2022-05-18 21:04:28 +0900

    Update and rename Untitled-1.py to FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,14 +2,14 @@ for num in range(1, 101):
     string = ''
 
     # ここから記述
-    if (num % 3 == 0):
+    if (num % 3 == 0) & (num % 5 == 0):
+        string = 'FizzBuzz'
+        
+    elif (num % 3 == 0):
         string = 'Fizz'
     
     elif (num % 5 == 0):
         string = 'Buzz' 
-    
-    elif (num % 3 == 0) & (num % 5 == 0):
-        string = 'FizzBuzz'
     
     else: 
         string = str(num)
